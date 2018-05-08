0d8b6626b55a53af25be9938d46f525354a236bc
UrosPurtic <uros.purtic@gmail.com>
2018-05-08 18:34:42 +0200

    zadatak 1

---
+++ src/components/NavBar.vue
@@ -0,0 +1,27 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                    <router-link class="navbar-brand" to="/"> Cars</router-link>
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <!--<li class="nav-item active">-->
+                    <!--<router-link class="nav-link" to="/cars"> Contacts</router-link>-->
+                <!--</li>-->
+            </ul>
+        </div>
+    </nav>
+</template>
+
+<script>
+    export default {
+        name: "NavBar"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/AppCars.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+
+        app cars
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "AppCars"
+    }
+</script>
+
+<style scoped>
+
+</style>
